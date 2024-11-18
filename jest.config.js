c14eb0e2bdf41ed4b2dff3858a4dcafa957d7ea2
egokt <erek@gktrk.com>
2024-11-17 21:28:06 -0800

    test: initialized tests, added tests for request-helpers (#3)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: "node",
+  transform: {
+    "\\.[jt]sx?$": ["ts-jest", { "useESM": true, }],
+  },
+  moduleNameMapper: {
+    "(.+)\\.js": "$1"
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+};
